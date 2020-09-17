eba25f0a6c70eeece518791504197ed0c195375b
Adapala <madhusudhan.adapala@capgemini.com>
2020-09-17 18:54:20 +0530

    samplekatalon

---
+++ MuleApiTest/Test Suites/JiraTestSuite/JiraTestSuite.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>JiraTestSuite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>39820ed1-99b6-4645-8644-450b47ae273b</testSuiteGuid>
+   <testCaseLink>
+      <guid>49477889-6b05-4583-8c34-af0ca8f7fdb6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JiraTestCase/JiraApiTest</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ef6e9da6-d86d-4997-b359-6ea222111ab4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/JiraTestCase/JiraNewApiTest</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ SampleAPITest/Test Suites/UserTestSuite/UserTS.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>UserTS</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>94715ddc-8a76-4179-aca7-6941658060ac</testSuiteGuid>
+   <testCaseLink>
+      <guid>70e4550f-6721-46dd-9b09-9c12c7043a60</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/NewUserTestCase/NewValidateUserDetails</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>012c7e13-0c7f-48be-b3a0-570f99c07329</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/UserTestCase/ValidateUserDetails</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>

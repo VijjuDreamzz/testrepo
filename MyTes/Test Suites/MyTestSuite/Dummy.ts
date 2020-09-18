47a4457324c3d62a087fb5edca2a53519cc03efc
Administrator <nowsudheer@gmail.com>
2020-09-18 13:43:14 +0530

    changes made by sujoy

---
+++ MyTes/Test Suites/MyTestSuite/Dummy.ts
@@ -0,0 +1,26 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Dummy</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>4b2ea4ca-fc16-4b54-ad9c-3c86fc29d46c</testSuiteGuid>
+   <testCaseLink>
+      <guid>7ef8f2e7-aa7c-48b6-8b73-dc67cdf62e90</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/MyTestCaseolder/TestUrl</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>7ebac7f3-6c18-44c9-90bd-d1be104f8314</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/MyTestCaseolder/WrongTestUrl</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
